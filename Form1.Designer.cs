1ae6b6081762c404e7ddec5e4389b9c2375082a6
jdunne <jdunne@tripplite.com>
2022-01-20 15:36:39 -0600

    Xrepo.(IronPython27).4 Manually merged in the form improvements from the ironpython34 branch.
    _Branch:IronPython27

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -148,6 +148,7 @@ namespace PythonScriptTest
             this.Name = "Form1";
             this.Text = "Python Script test";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
